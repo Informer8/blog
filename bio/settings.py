65a0ca23c9cf3bf6049f2f580b0a3cb0e82dc595
Chhabi Raman Adhikari <chhabi.adhikari@rts.com.np>
2023-06-23 14:59:15 +0545

    updated working versionm

--- bio/settings.py
+++ bio/settings.py
@@ -28,13 +28,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-r91@$vr&rxue(l=9-ywf#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
-
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-
 # if RENDER_EXTERNAL_HOSTNAME:    
 #     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-# ALLOWED_HOSTS = ['craman.com.np']
+ALLOWED_HOSTS = ['craman.com.np']
 
 # Application definition
 INSTALLED_APPS = [
